31b03bee162d7c2c0dbc6781719fc682784cd22e
Jef <Jacobsjef16@gmail.com>
2022-12-23 22:22:49 +0100

    extra nmap profile

--- ontology/tools.pl
+++ ontology/tools.pl
@@ -1,11 +1,15 @@
 tool(nmap, nmap_1).
+tool(nmap, nmap_2).
+
 tool(hydra, hydra_ssh).
 tool(hydra, hydra_ftp).
 tool(hydra, hydra_telnet).
+
 tool(gobuster, gobuster_http).
 tool(gobuster, gobuster_https).
 
 profile(nmap_1, [ip=IP]) :- ip(IP).
+profile(nmap_2, [ip=IP,port=P]) :- ip(IP), devicePorts(IP,P).
 
 profile(hydra_ssh, [ip=IP, port=P]) :- ip(IP), deviceServices(IP, P, 'ssh').
 profile(hydra_telnet, [ip=IP, port=P]) :- ip(IP), deviceServices(IP, P, 'telnet').
@@ -19,6 +23,9 @@ format_command(Template, Parameters, Command) :-
 
 nmap_1(Parameters, Command) :-
     format_command('nmap -F ~w', [Parameters.ip], Command).
+
+nmap_2(Parameters, Command) :-
+    format_command('nmap -sC -sV -P ~w ~w', [Parameters.port, Parameters.ip], Command).
 
 hydra_ssh(Parameters, Command) :-
     format_command('hydra -C wordlists/default_credentials/test.txt ~w ssh' , [Parameters.ip], Command).
