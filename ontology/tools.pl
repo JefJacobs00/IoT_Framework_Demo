bd47b8c305427ddd614c870b5f38a5de63165726
jefjacobs00 <jacobsjef16@gmail.com>
2023-03-09 18:44:20 +0100

    Prolog get the lowest avg duration profile

--- ontology/tools.pl
+++ ontology/tools.pl
@@ -3,33 +3,12 @@ tools(Tool, Profile, Command) :-
     call(Profile, Parameters, Command),
     tool(Tool, Profile).
 
-test(Profiles) :-
-    findall(Profile, profile(Profile, Parameters), Profiles),
-    do(Profiles).
+test(Profile, Min) :-
+    findall(P, profile(P, _), Profiles),
+    getLowest(Profiles, Profile, Min).
 
-do([]).
-do([H|T]) :-
-    write(H),
+getLowest([], _, 9999999).
+getLowest([H|T], Profile, Min) :-
     avgProfileDuration(H, Avg),
-    write(Avg),
-    do(T).
-
-tools_duration(Tool, Profile, Command) :-
-    lowest_duration(Profile, Duration),
-    tools(Tool, Profile, Command).
-
-lowest_duration(Profile, Min) :-
-    findall(Avg, avgProfileDuration(Profile, Avg), Averages),
-    list_min(Averages, Min),
-     avgProfileDuration(Profile, Min).
-
-format_command(Template, Parameters, Command) :-
-    format(string(Command), Template, Parameters).
-
-list_min([L|Ls], Min) :-
-    list_min(Ls, L, Min).
-
-list_min([], Min, Min).
-list_min([L|Ls], Min0, Min) :-
-    Min1 is min(L, Min0),
-    list_min(Ls, Min1, Min).
+    getLowest(T, NewProfile, Min1),
+    (Avg < Min1 -> (Min = Avg, Profile = H) ; (Min = Min1, Profile = NewProfile)).
