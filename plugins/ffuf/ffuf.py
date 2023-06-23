cd68c0e2db03c148b86e74a6f0093a004484fbff
jefjacobs00 <jacobsjef16@gmail.com>
2023-06-23 16:15:47 +0200

    fixing ontology bug

--- plugins/ffuf/ffuf.py
+++ plugins/ffuf/ffuf.py
@@ -2,7 +2,7 @@ from plugins.tool import Tool
 
 class Ffuf(Tool):
     def __init__(self, ontology):
-        self.parserDict = {'http_param_fuzz': {'webParameterName': ' FUZZ: '}, 'http_param_input_fuzz': {"webParameterInput": ' FUZZ: '}}
+        self.parserDict = {'http_param_fuzz': {'webParameterName': ' FUZZ: '}, 'http_param_input_fuzz': {"inputValue": ' FUZZ: '}}
         self.parser = self.parserDict["http_param_fuzz"]
         line_start = '    *'
         super().__init__(ontology=ontology, parser=self.parser, info_start=line_start)
